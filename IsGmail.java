475a766447d90e1ffe583fc7639353bcd3147dab
Annitra <anuta07.05@gmail.com>
2017-06-25 17:46:36 +0300

    Validation: Have all mails from list domen gmail?

---
+++ IsGmail.java
@@ -0,0 +1,16 @@
+import java.util.regex.Pattern;
+
+/**
+ * Created by User on 07.06.2017.
+ */
+public class IsGmail {
+    public static void main(String[] args) {
+        String input = "email1@gmail.com, email2@gmail.com, email3@gmail.com," +
+                " email4@gmail.com, email5@gmail.com, email6@gmail.com,";
+        System.out.println(isGmail(input));
+    }
+    public static boolean isGmail(String input)
+    {
+        return Pattern.matches("^(?i)(?:(?:[^@]+@gmail\\.com)(?:,|$))+$", input);
+    }
+}
